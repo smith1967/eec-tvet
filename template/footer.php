ec4f3f66e546e4a2b3ba2b0617ea72b62b83656c
unknown <smith.sookkhi@gmail.com>
2018-03-21 23:58:09 +0700

    Update Header and footer

--- template/footer.php
+++ template/footer.php
@@ -211,6 +211,25 @@
 <!-- DataTables -->
 <script src="asset/AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="asset/AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<!-- Select2 -->
+<script src="asset/AdminLTE/bower_components/select2/dist/js/select2.full.min.js"></script>
+<!-- InputMask -->
+<script src="asset/AdminLTE/plugins/input-mask/jquery.inputmask.js"></script>
+<script src="asset/AdminLTE/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="asset/AdminLTE/plugins/input-mask/jquery.inputmask.extensions.js"></script>
+<!-- date-range-picker -->
+<script src="asset/AdminLTE/bower_components/moment/min/moment.min.js"></script>
+<script src="asset/AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap datepicker -->
+<script src="asset/AdminLTE/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- bootstrap color picker -->
+<script src="asset/AdminLTE/bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+<!-- bootstrap time picker -->
+<script src="asset/AdminLTE/plugins/timepicker/bootstrap-timepicker.min.js"></script>
+<!-- SlimScroll -->
+<script src="asset/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- iCheck 1.0.1 -->
+<script src="asset/AdminLTE/plugins/iCheck/icheck.min.js"></script>
 <!-- FastClick -->
 <script src="asset/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
